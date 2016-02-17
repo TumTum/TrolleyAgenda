db5374e4f7e13f584f1737998ec4537219713a7f
Tobias Matthaiou <developer@tobimat.eu>
2016-02-17 19:26:01 +0100

    Menu Eingebaut

---
+++ src/Trolly/AgendaBundle/Menu/Builder.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created for TrollyAgenda
+ * Author: Tobias Matthaiou <matthaiou@tobimat.eu>
+ * Date: 17.02.16
+ * Time: 19:18
+ * Copyright: 2014 Tobias Matthaiou
+ */
+
+namespace Trolly\AgendaBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Builder implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+
+        $menu->addChild('Home', array('route' => 'trolly_agenda_default_homepage'));
+
+//        // create another menu item
+//        $menu->addChild('About Me', array('route' => 'about'));
+//        // you can also add sub level's to your menu's as follows
+//        $menu['About Me']->addChild('Edit profile', array('route' => 'edit_profile'));
+
+        // ... add more children
+
+        return $menu;
+    }
+}
