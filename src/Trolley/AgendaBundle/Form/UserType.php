506d92558a49aeb6dfc9b4b787f462086e9c3366
Tobias Matthaiou <developer@tobimat.eu>
2016-04-15 21:31:44 +0200

    PHP 5.6 kann keine groupierung.

--- src/Trolley/AgendaBundle/Form/UserType.php
+++ src/Trolley/AgendaBundle/Form/UserType.php
@@ -7,12 +7,10 @@ use FOS\UserBundle\Form\Type\GroupFormType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\{
-    TextType,
-    ChoiceType,
-    EmailType,
-    PasswordType
-};
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
