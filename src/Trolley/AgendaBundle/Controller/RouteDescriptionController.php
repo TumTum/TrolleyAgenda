ae1fdddabd44d5cf962b57a1a9e6de23196f4701
Tobias Matthaiou <developer@tobimat.eu>
2016-04-18 09:20:35 +0200

    Security eingebaut

--- src/Trolley/AgendaBundle/Controller/RouteDescriptionController.php
+++ src/Trolley/AgendaBundle/Controller/RouteDescriptionController.php
@@ -6,12 +6,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Trolley\AgendaBundle\Entity\RouteDescription;
 use Trolley\AgendaBundle\Form\RouteDescriptionType;
 
 /**
  * RouteDescription controller.
  *
+ * @Security("has_role('ROLE_ADMIN')");
  * @Route("/route-description")
  */
 class RouteDescriptionController extends Controller
@@ -62,8 +64,9 @@ class RouteDescriptionController extends Controller
     /**
      * Finds and displays a RouteDescription entity.
      *
-     * @Route("/{id}")
+     * @Route("/{id}.html")
      * @Method("GET")
+     * @Security("has_role('ROLE_USER')");
      */
     public function showAction(RouteDescription $routeDescription)
     {
