e7d929d2f1f743e8c8020434802b40dba1d9e50c
Tobias Matthaiou <developer@tobimat.eu>
2016-04-18 09:14:09 +0200

    Neues CRUD Routen Beschreibung

---
+++ src/Trolley/AgendaBundle/Entity/RouteDescription.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Trolley\AgendaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RouteDescription
+ *
+ * @ORM\Table(name="route_description")
+ * @ORM\Entity(repositoryClass="Trolley\AgendaBundle\Repository\RouteDescriptionRepository")
+ */
+class RouteDescription
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="html", type="text", nullable=true)
+     */
+    private $html;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return RouteDescription
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set html
+     *
+     * @param string $html
+     *
+     * @return RouteDescription
+     */
+    public function setHtml($html)
+    {
+        $this->html = $html;
+
+        return $this;
+    }
+
+    /**
+     * Get html
+     *
+     * @return string
+     */
+    public function getHtml()
+    {
+        return $this->html;
+    }
+}
+
