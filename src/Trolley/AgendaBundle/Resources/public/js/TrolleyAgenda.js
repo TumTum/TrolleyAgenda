9c4cbcf6eb84e9e17f96f93908f03bf50d2b8dc9
Tobias Matthaiou <developer@tobimat.eu>
2016-05-08 08:16:29 +0200

    Neues JavaScript Main Script hinzugefügt

---
+++ src/Trolley/AgendaBundle/Resources/public/js/TrolleyAgenda.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tobi on 08.05.16.
+ */
+
+function TrolleyAgenda() {}
+
+TrolleyAgenda.prototype.run = function() {};
+
+$(function(){
+    var trolleyAgenda = new TrolleyAgenda();
+    trolleyAgenda.run();
+});
