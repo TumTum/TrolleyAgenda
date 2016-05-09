5298590b7ba6ce889796478f964a890ac52f4eae
Tobias Matthaiou <developer@tobimat.eu>
2016-05-09 20:42:42 +0200

    phpunit test verschoben in phpspec

--- src/Trolley/AgendaBundle/Tests/Entity/DayTest.php
+++ src/Trolley/AgendaBundle/Tests/Entity/DayTest.php
@@ -23,37 +23,7 @@ class DayTest extends KernelTestCase
     use createUserDayRelationshipsTrait;
 
     /**
-     * Test ob das Object ein Datum zurück gibt Day
-     */
-    public function testDayToString()
-    {
-        $day = new Day('2016-10-20');
-
-        $this->assertEquals('2016-10-20', (string)$day);
-    }
-
-    /**
-     * Testet das format
-     */
-    public function testFormatDate()
-    {
-        $day = new Day('2016-10-20');
-
-        $this->assertEquals('10-2016~20', $day->format('m-Y~d'));
-    }
-
-    /**
-     * Testet das format
-     */
-    public function testFormatMonth()
-    {
-        $day = new Day('2016-10-20');
-
-        $this->assertEquals('October', $day->getMonthName());
-    }
-
-    /**
-     * Fügt einen User zum Day
+     * Testet es mit der DB den Tag mit den User
      */
     public function testAddUserToDay()
     {
@@ -75,104 +45,6 @@ class DayTest extends KernelTestCase
         foreach ($dayDB->getTaUsers() as $userDB) {
             $this->assertContains('testuser', $userDB->getUsername());
         }
-    }
-
-    /**
-     * Löscht den User vom Tag wieder
-     */
-    public function testRemoveUserFromDay()
-    {
-        /**
-         * @var User $user
-         * @var User $user2
-         * @var User $userFromDay
-         * @var Day $day
-         */
-        list($day, $user, $user2) = $this->createDayTowUsers();
-
-        $day->removeUser($user);
-
-        foreach ($day->getTaUsers() as $userFromDay) {
-            $this->assertNotEquals($user->getUsername(), $userFromDay->getUsername());
-        }
-    }
-
-    /**
-     * Prüft ob der User gehen darf
-     */
-    public function testUserCanNotGo()
-    {
-        $day = new Day('2014-02-03');
-
-        $user = new User();
-        $user->setUsername('UserCanNotGo');
-
-        $this->assertFalse($day->canUserGo($user));
-    }
-
-    /**
-     * Der User kann gehen
-     */
-    public function testUserCanGo()
-    {
-        $day = new Day('2014-02-03');
-
-        $user = new User();
-        $user->setUsername('UserCanGo');
-
-        $day->userAcceptToGo($user);
-
-        $this->assertTrue($day->canUserGo($user));
-    }
-
-    /**
-     * Der User kann nicht gehen
-     *
-     * @covers Trolley\AgendaBundle\Entity\Day::userCancelToGo
-     */
-    public function testCancelUserToGo()
-    {
-        $day = new Day('2014-02-03');
-
-        $user = new User();
-        $user->setUsername('UserCanGo');
-
-        $day->userAcceptToGo($user);
-        $day->userCancelToGo($user);
-
-        $this->assertFalse($day->canUserGo($user));
-    }
-
-    /**
-     * Der User konnte gehen, wurde gecancelt.
-     * Daraufhin bewarb er sich erneut, und bekommt den nicht angenommen Status
-     */
-    public function testUsersCouldGoWasCanceledCompetitionRenewed()
-    {
-        $day = new Day('2014-02-03');
-
-        $user = new User();
-        $user->setUsername('UserCanGo');
-
-        $day->addUser($user);
-        $day->userAcceptToGo($user);
-
-        $day->removeUser($user);
-        $day->addUser($user);
-
-        $this->assertFalse($day->canUserGo($user));
-
-    }
-
-    public function testDoubleUserInDay()
-    {
-        $day = $this->createOneDay();
-        $user = $this->createUser('doubleuser');
-
-        $day->addUser($user);
-        $day->addUser($user);
-
-        $this->assertCount(1, $day->getTaUsers());
     }
 
     /**
