d80545f0358eb5022adcd821e12ec93a79b90670
Tobias Matthaiou <developer@tobimat.eu>
2016-05-11 14:16:06 +0200

    (dev) Moeglichkeit jedem User seine Einladungs Karte auszudrucken

--- src/Trolley/AgendaBundle/Controller/InfoCardsController.php
+++ src/Trolley/AgendaBundle/Controller/InfoCardsController.php
@@ -10,12 +10,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
  *
  * @package Trolley\AgendaBundle\Controller
  *
- * @Route("/users")
+ * @Route("/print/cards")
  */
 class InfoCardsController extends Controller
 {
     /**
-     * @Route("/all-cards")
+     * @Route("/")
      */
     public function allUserCardsAction()
     {
@@ -23,6 +23,18 @@ class InfoCardsController extends Controller
         $host = $_SERVER['HTTP_HOST'];
         return $this->render('TrolleyAgendaBundle:InfoCards:all_user_cards.html.twig', [
             'host' => $host,
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * @Route("/cover")
+     */
+    public function coverCardsAction()
+    {
+        $users = $this->getDoctrine()->getRepository('TrolleyAgendaBundle:User')->findAll();
+        $host = $_SERVER['HTTP_HOST'];
+        return $this->render('TrolleyAgendaBundle:InfoCards:cover_cards.html.twig', [
             'users' => $users
         ]);
     }
